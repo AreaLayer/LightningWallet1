184cca0d9a3a6f1ad6b76ea7f830ee72338db673
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-09-12 07:55:29 -0300

    Update TRADE_TICKERS.ts

--- src/core/swap.app/constants/TRADE_TICKERS.ts
+++ src/core/swap.app/constants/TRADE_TICKERS.ts
@@ -1,14 +1,8 @@
 import { NATIVE, ETH_TOKENS, BNB_TOKENS, MATIC_TOKENS } from './COINS'
 
 export default [
-  'ETH-BTC',
-  'ETH-GHOST',
-  'ETH-NEXT',
-  'BNB-BTC',
-  'ARBETH-BTC',
+  'BTC⚡️ -BTC',
 
-  ...Object.values(ETH_TOKENS).map(token => `{ETH}${token}-BTC`),
-  ...Object.values(BNB_TOKENS).map(token => `{BNB}${token}-BTC`),
-  ...Object.values(MATIC_TOKENS).map(token => `{MATIC}${token}-BTC`),
+  ...Object.values(BTC_TOKENS).map(token => `{BTC⚡️ }${token}-BTC`),
   // ...Object.values(ETH_TOKENS).map(token => `${token}-USDT`),
 ]
