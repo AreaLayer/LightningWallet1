09ceb7b980734c852c82040d196ffe531bdea25a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-09-29 11:55:04 -0300

    Update lightning.ts

--- src/common/utils/coin/lightning.ts
+++ src/common/utils/coin/lightning.ts
@@ -1,1 +1,7 @@
 
+import apiLooper from '../apiLooper'
+import { BigNumber } from 'bignumber.js'
+import * as bitcoin from 'bitcoinjs-lib'
+import typeforce from 'swap.app/util/typeforce'
+import constants from 'common/helpers/constants'
+import { requestProvider } from 'webln';
