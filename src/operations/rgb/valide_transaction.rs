c854a588174fb50427a4b6f87bf3f0e1b6b32428
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-12-05 12:33:31 -0300

    Create valide_transaction.rs

---
+++ src/operations/rgb/valide_transaction.rs
@@ -0,0 +1,20 @@
+se anyhow::Result;
+
+use crate::{
+    data::{constants::url, structs::ValidateRequest},
+    log,
+    util::post_json,
+};
+
+pub async fn validate_transfer(consignment: String, node_url: Option<String>) -> Result<()> {
+    //TODO: review
+    let validate_request = ValidateRequest { consignment };
+
+    let (response, _) = post_json(url("validate", &node_url), &validate_request).await?;
+
+    // parse into generic JSON value
+    // let result = serde_json::from_str(&response)?;
+
+    log!(format!("validate_transfer result {response:?}"));
+    Ok(())
+}
