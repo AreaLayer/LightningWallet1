7ceb0bf999f8bc874a73bdd0acee50521e190ae3
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-12-05 17:02:15 -0300

    Added Segwit and Taproot

--- src/operations/bitcoin/balance.rs
+++ src/operations/bitcoin/balance.rs
@@ -1,7 +1,7 @@
 use std::rc::Rc;
 
 use anyhow::Result;
-use bdk::{blockchain::esplora::EsploraBlockchain, database::MemoryDatabase, SyncOptions, Wallet};
+use bdk::{blockchain::esplora::EsploraBlockchain, database::MemoryDatabase, SyncOptions, Wallet, Taproot};
 
 use crate::{
     data::{
@@ -47,3 +47,19 @@ pub async fn synchronize_wallet(wallet: &Wallet<MemoryDatabase>) -> Result<()> {
     log!("Synced");
     Ok(())
 }
+
+pub trait ExtScriptContext: ScriptContext {
+    /// Returns the [`ScriptContext`] as a [`ScriptContextEnum`]
+    fn as_enum() -> ScriptContextEnum;
+
+    }
+
+    /// Returns whether the script context is [`Segwitv0`](miniscript::Segwitv0)
+    fn is_segwit_v0() -> bool {
+        Self::as_enum().is_segwit_v0()
+    }
+
+    /// Returns whether the script context is [`Tap`](miniscript::Tap), aka Taproot or Segwit V1
+    fn is_taproot() -> bool {
+        Self::as_enum().is_taproot()
+    }
