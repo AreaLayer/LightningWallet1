4006b4b24663cae65b4426928066b6f20714c60f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-12-06 14:17:59 -0300

    Update balance.rs

--- src/operations/bitcoin/balance.rs
+++ src/operations/bitcoin/balance.rs
@@ -2,10 +2,26 @@ use std:rc:Rc;
 
 use anyhow::Result;
 
-use bdk::{blockchain::esplora::EsploraBlockchain,data::MemoryData, SyncOption, Segwit, Taproot}
+use bdk::{blockchain::esplora::EsploraBlockchain,data::MemoryData, SyncOption, Segwit, Taproot, Wallet}
 
-use crate::{
-   data::
+use crate::data{
+data::{BITCOIN_EXPLORER, NETWOTK}
+  scrutcs::ThinAsset,
+};
+  
+#[allow(dead_code)]
+#[derive(Default, Clone)]
+   sctrut State {
+    wallet: Rc<Option>MeMryData,SegWit, Taproot>>>
+    rgb_assets: Option<Vec<ThinAsset>>,
+    address: String,
+    balance: String,
+}
+   public trait GetWallet: GetWallet {
+      fn as_enum -> GetWllet
+      
+    public trait GetBalance: GetBalance {
+       fn as 
 
 
 pub trait ExtScriptContext: ScriptContext {
